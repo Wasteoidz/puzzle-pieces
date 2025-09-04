924b22fd28cf021cc06ff2371c8dc6ca18dd46b6
Wasteoidz <Rolf91@live.com>
2025-09-04 12:47:49 +0200

    changes
    added puzzelbox, fixed names for pictures to line up with array

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,4 @@
 //Model
-let theBoard;
-let pieces = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36];
-
 
 
 
@@ -11,14 +8,13 @@ showView()
 function showView(){
     document.getElementById('app').innerHTML = /**html**/ `
     <h1 class="header">Her skal det pussles!</h1>
-    <div class="board" id="board"></div> <br/>
-    <button onclick=getPieces(36)>Start Spill</button>
-    
-    
-    
-    
-    
-    
+    <div class="board" 
+        id="board" 
+        ondragover="allowDrop(event)" 
+        ondrop="dropPiece(event)"
+    ></div> <br/>
+    <button onclick=getPieces(36)>Nytt spill</button> <br/><br/>
+    <div class="puzzleBox" id="puzzleBox"></div>
     
     `;
 }
@@ -32,9 +28,11 @@ function showView(){
 //   Controller
 
 function getPieces(times) {
+    pieces = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36];
     showView()
     for (let i = 0; i <times; i++) {
-        document.getElementById('board').innerHTML += `<div class="pieces">${randomPiece()}</div>`;
+        document.getElementById('puzzleBox').innerHTML += `<div class="pieces">${randomPiece()}</div>`;
+        
     }    
  
 }
@@ -42,7 +40,15 @@ function getPieces(times) {
 function randomPiece() {
     let numberOfPieces = pieces.length;
     let randomPiecePosition = Math.floor(Math.random() * numberOfPieces);
-    let randomPieces = pieces[randomPiecePosition];
-    return randomPieces;
-
+    let pieceNumber = pieces[randomPiecePosition];
+    pieces.splice(randomPiecePosition, 1);
+    return `<img 
+        src="CSS/bilder/${pieceNumber}.jpg" 
+        alt="Piece ${pieceNumber}" 
+        class="piece-img"
+        id="piece-${pieceNumber}"
+        draggable = "true"
+        ondragstart="dragStart(event, ${pieceNumber})"
+        />`;    
 }
+
