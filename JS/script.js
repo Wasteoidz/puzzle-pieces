01cb43d73a1d163e17b4b7e983c4be2104b31479
Wasteoidz <Rolf91@live.com>
2025-09-05 11:46:30 +0200

    fixed multiple puzzles and made the work

--- JS/script.js
+++ JS/script.js
@@ -14,9 +14,9 @@ function showView(){
         ondragover="allowDrop(event)" 
         ondrop="drop(event)"
     ></div> <br/>
-    <button onclick=getPieces(36)>&#127752; Enhjørning &#128052;</button>
-    <button onclick=getPieces(36)>&#128571; Gabby &#128571;</button>
-    <button onclick=getPieces(36)>&#128054; Paw Patrol &#128054;</button><br/><br/>
+    <button onclick="getPieces(36,'unicorn')">&#127752; Enhjørning &#128052;</button>
+    <button onclick="getPieces(36,'gabby')">&#128571; Gabby &#128571;</button>
+    <button onclick="getPieces(36,'pawPatrol')">&#128054; Paw Patrol &#128054;</button><br/><br/>
     <div class="puzzleBox" id="puzzleBox"></div>
     
     `;
@@ -30,11 +30,11 @@ function showView(){
 
 //   Controller
 
-function getPieces(times) {
+function getPieces(times, puzzleFolder) {
     pieces = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36];
     showView()
     for (let i = 0; i <times; i++) {
-        document.getElementById('puzzleBox').innerHTML += `<div class="pieces">${randomPiece()}</div>`;
+        document.getElementById('puzzleBox').innerHTML += `<div class="pieces">${randomPiece(puzzleFolder)}</div>`;
         document.getElementById('board').innerHTML += `<div 
                                                         id="emptyBox" 
                                                         class="emptyBox" 
@@ -46,13 +46,13 @@ function getPieces(times) {
  
 }
 
-function randomPiece() {
+function randomPiece(puzzleFolder) {
     let numberOfPieces = pieces.length;
     let randomPiecePosition = Math.floor(Math.random() * numberOfPieces);
     let pieceNumber = pieces[randomPiecePosition];
     pieces.splice(randomPiecePosition, 1);
     return `<img 
-        src="CSS/bilder/unicorn/${pieceNumber}.jpg" 
+        src="CSS/bilder/${puzzleFolder}/${pieceNumber}.jpg" 
         alt="Piece ${pieceNumber}" 
         class="piece-img"
         id="piece-${pieceNumber}"
