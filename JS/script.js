759446a86a5fd7a3cfc0d876b221664d652c45e8
Wasteoidz <Rolf91@live.com>
2025-09-05 13:36:57 +0200

    some CSS

--- JS/script.js
+++ JS/script.js
@@ -8,16 +8,15 @@
 showView()
 function showView(){
     document.getElementById('app').innerHTML = /**html**/ `
-    <h1 class="header">Her skal det pusles!</h1>
-    <div class="board" 
+        <div class="board" 
         id="board" 
         ondragover="allowDrop(event)" 
         ondrop="drop(event)"
-    ></div> <br/>
-    <button onclick="getPieces(36,'unicorn')">&#127752; Enhjørning &#128052;</button>
-    <button onclick="getPieces(36,'gabby')">&#128571; Gabby &#128571;</button>
-    <button onclick="getPieces(36,'pawPatrol')">&#128054; Paw Patrol &#128054;</button><br/><br/>
-    <div class="puzzleBox" id="puzzleBox"></div>
+    ><h1 id="tempPuzzleText"> &#127775;Her skal det pusles! &#127775;</h1></div> <br/>
+    <button class="knapp" onclick="getPieces(36,'unicorn')">&#127752; Enhjørning &#129412;</button>
+    <button class="knapp" onclick="getPieces(36,'gabby')">&#128571; Gabby &#128571;</button>
+    <button class="knapp" onclick="getPieces(36,'pawPatrol')">&#128054; Paw Patrol &#128054;</button><br/><br/>
+    <div class="puzzleBox" id="puzzleBox"><h1 id="tempBoxText">&#128525; Her finner du puslebitene &#128525;</h1></div>
     
     `;
 }
@@ -31,6 +30,8 @@ function showView(){
 function getPieces(times, puzzleFolder) {
     pieces = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36];
     showView()
+    document.getElementById('tempPuzzleText').style.display = "none";
+    document.getElementById('tempBoxText').style.display = "none";
     for (let i = 0; i <times; i++) {
         document.getElementById('puzzleBox').innerHTML += `<div class="pieces">${randomPiece(puzzleFolder)}</div>`;
         document.getElementById('board').innerHTML += `<div 
